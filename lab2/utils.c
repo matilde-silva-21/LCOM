a94ac2a52d01a7275f761de8840dc04da1dd9383
beatrizfontao <anabfontao@gmail.com>
2022-03-24 19:59:52 +0000

    lab2 exercices 1 and 2 done, not tested

--- lab2/utils.c
+++ lab2/utils.c
@@ -2,34 +2,37 @@
 
 #include <stdint.h>
 
-int (util_get_LSB)(uint16_t val, uint8_t *lsb) {
-    /* To be implemented by the students */
-    printf("%s is not yet implemented!\n", __func__);
+int(util_get_LSB)(uint16_t val, uint8_t *lsb) {
 
+  if (lsb == NULL)
     return 1;
+
+  *lsb = (uint8_t) val;
+
+  return 0;
 }
 
-int (util_get_MSB)(uint16_t val, uint8_t *msb) {
-    /* To be implemented by the students */
-    printf("%s is not yet implemented!\n", __func__);
+int(util_get_MSB)(uint16_t val, uint8_t *msb) {
 
+  if (msb == NULL)
     return 1;
+
+  *msb = (uint8_t) (val >> 8);
+
+  return 0;
 }
 
-int (util_sys_inb)(int port, uint8_t *value) {
-    /* To be implemented by the students */
+int(util_sys_inb)(int port, uint8_t *value) {
 
-    printf("util_sys_inb\n");
+  if (value == NULL)
+    return 1;
 
-    if (value == NULL)
-        return 1;
+  uint32_t temp = 0x0;
 
-    uint32_t temp = 0x0;
+  if (sys_inb(port, &temp))
+    return 1;
 
-    if(sys_inb(port, &temp))
-        return 1;
+  *value = (uint8_t) temp;
 
-    *value = (uint8_t)temp;
-
-    return 0;
+  return 0;
 }
